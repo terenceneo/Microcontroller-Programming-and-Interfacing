0e3da7e381415d0b0eac9c65f0cb1d574e41cfd5
terenceneo <43961820+terenceneo@users.noreply.github.com>
2018-11-10 22:22:00 +0800

    Driver file changes

--- Assignment 2 Workspace/Lib_EaBaseBoard/inc/joystick.h
+++ Assignment 2 Workspace/Lib_EaBaseBoard/inc/joystick.h
@@ -9,11 +9,11 @@
 #define __JOYSTICK_H
 
 
-#define JOYSTICK_CENTER 0x01
-#define JOYSTICK_UP     0x02
-#define JOYSTICK_DOWN   0x04
-#define JOYSTICK_LEFT   0x08
-#define JOYSTICK_RIGHT  0x10
+#define JOYSTICK_CENTER 0x01 //1<<0
+#define JOYSTICK_UP     0x02 //1<<1
+#define JOYSTICK_DOWN   0x04 //1<<2
+#define JOYSTICK_LEFT   0x08 //1<<3
+#define JOYSTICK_RIGHT  0x10 //1<<4
 
 
 void joystick_init (void);
