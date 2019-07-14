d43b0dece19fa9b498efe2e21c555a8c1bdd53f5
Terence Neo <terencenyy@yahoo.com.sg>
2019-07-14 22:17:48 +0800

    Remove from dropbox

--- a2/Lib_EaBaseBoard/Debug/src/font5x7.d
+++
@@ -1,4 +0,0 @@
-src/font5x7.d: ../src/font5x7.c \
- C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_EaBaseBoard\inc/font_macro.h
-
-C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_EaBaseBoard\inc/font_macro.h:
--- a2/Lib_MCU/Debug/src/lpc17xx_libcfg_default.d
+++
@@ -1,7 +0,0 @@
-src/lpc17xx_libcfg_default.d: ../src/lpc17xx_libcfg_default.c \
- C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h \
- C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_MCU\inc/lpc_types.h
-
-C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h:
-
-C:\Users\Rachel\Documents\Year\ 2\ Sem\ 1\EE2028\assignment2\Lib_MCU\inc/lpc_types.h:
